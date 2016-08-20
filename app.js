452c595585661e8205e840f861d14bdb60cf950c
Pavel Gordeyev <gordeyev.p@gmail.com>
2016-08-20 14:34:47 -0400

    Get file and return its name

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 8080;
-const hostname = '127.0.0.1';
+// Sets port & hostname if running on either heroku or local machine
+const port = parseInt(process.env.PORT, 10) || 8080;
+const hostname = parseInt(process.env.PORT, 10) ? '0.0.0.0' : '127.0.0.1';
 
-app.use('/static', express.static(__dirname + '/scripts'));
+
+app.use('/static', express.static(__dirname + './public'));
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+		extended: true
+}));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res){
+app.get('/form', function(req, res){
 	res.render('index');
 });
 
+app.post('/form', function(req,res){
+	console.log("file:",req.body);
+	res.send({status: 'SUCCESS'});
+});
+
 app.listen(port, hostname, function() {
-	console.log("The frontend server is running on port %d!",port);
+	console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
 });
